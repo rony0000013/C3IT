9182d4c0fec30d268fbd838cbbf0b71b946a732b
candy12345678910 <93834243+candy12345678910@users.noreply.github.com>
2024-09-23 20:29:15 +0530

    Made schedule

--- src/component/Schedule/Schedule.js
+++ src/component/Schedule/Schedule.js
@@ -1,14 +1,55 @@
 import React from 'react'
-import Heading from '../Heading'
+// import Heading from '../Heading'
+import { IoMdDownload } from "react-icons/io";
+import "./Schedule.css"
+
 const Schedule = () => {
   return (
-    <div className='mt-5' style={{marginBottom: "8%"}}>
-    {<Heading>schedule</Heading>}
-    <div style={{width:"100%",height:"440px",display:'flex',justifyContent:'center'}}>
-      <h1>Will be updated soon..</h1>
-    </div>
-   </div>
-  )
+    <>
+      <br/>
+      <div className="container">
+        {/* Program Schedule Heading */}
+        <h2 className="heading">Program Schedule</h2>
+
+        {/* Program Schedule iframe */}
+        <div className="iframe-container">
+          <iframe 
+            title="brochure" 
+            src="https://drive.google.com/file/d/1EhpEDwWfr0TFmiLBYnYj3tMLl7eWh2Wj/preview" 
+            className="iframe" 
+            allow="autoplay">
+          </iframe>
+
+          <a href='Program Schedule C3IT 2024.pdf' download="Program Schedule C3IT 2024" className="download-btn">
+            <IoMdDownload className='download-icon'/>
+            {/* <p className='download-icon'>Download PDF</p> */}
+          </a>
+
+        </div>
+        <br/>
+        <br/>
+        <div className='saperator'></div>
+        {/* Technical Schedule Heading */}
+        <h2 className="heading">Technical Schedule</h2>
+
+        {/* Technical Session Schedule iframe */}
+      
+        <div className="iframe-container">
+          <iframe 
+            title="brochure" 
+            src="https://drive.google.com/file/d/1uZK2i7caAcius2KEMBQCN7_xH1fZeX04/preview" 
+            className="iframe" 
+            allow="autoplay">
+          </iframe>
+          <a href='Technical Session Schedule C3IT-2024.pdf' download="Technical Session Schedule C3IT-2024" className="download-btn">
+            {/* <p className='download-icon'>Download PDF</p> */}
+            <IoMdDownload className='download-icon'/>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+  
 }
 
 export default Schedule
